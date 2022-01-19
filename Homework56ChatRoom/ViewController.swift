9fdb9deb95c79dd632b1b6e887a511b4afa327d5
Poga Huang <pgh86812@gmail.com>
2022-01-19 09:25:38 +0800

    Initial Commit

---
+++ Homework56ChatRoom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework56ChatRoom
+//
+//  Created by 黃柏嘉 on 2022/1/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
